3d77011fd7144829f2dd188ad4a8a61c6d528cbb
nuintun <nuintun@qq.com>
2016-06-12 18:01:03 +0800

    update files

--- examples/css/selectbox.css.js
+++ examples/css/selectbox.css.js
@@ -1,4 +1,4 @@
 define("css/selectbox.css.js", ["import-style"], function(require, exports, module){
 var style = require("import-style");
-style.css(".ui-beauty-select {\n  display: inline-block;\n  *display: inline; *zoom: 1;\n  position: relative;\n  margin: 0; padding: 0;\n  border: 1px solid #ccc;\n  box-sizing: content-box;\n}\n");
+style.css(".ui-beauty-select {\n  display: inline-block;\n  *display: inline; *zoom: 1;\n  position: relative;\n  margin: 0; padding: 0;\n  border: 1px solid #ccc;\n  background: url(./examples/images/select.png) no-repeat right center;\n}\n");
 });
