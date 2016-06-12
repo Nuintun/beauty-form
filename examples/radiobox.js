58dd7ba0ce4eb6fa5d69f070824b86579f551d76
nuintun <nuintun@qq.com>
2016-06-12 17:06:47 +0800

    update files

--- examples/radiobox.js
+++ examples/radiobox.js
@@ -1,6 +1,11 @@
 define("radiobox", ["./css/radiobox.css.js","./choice"], function(require, exports, module){
 /**
- * Created by nuintun on 2015/3/31.
+ * radio
+ * Date: 2015/6/7
+ * https://github.com/nuintun/beauty-form
+ *
+ * This is licensed under the MIT License (MIT).
+ * For details, see: https://github.com/nuintun/beauty-form/blob/master/LICENSE
  */
 
 'use strict';
--- src/checkbox.js
+++ src/checkbox.js
@@ -1,5 +1,10 @@
 /**
- * Created by nuintun on 2015/3/31.
+ * checkbox
+ * Date: 2015/6/7
+ * https://github.com/nuintun/beauty-form
+ *
+ * This is licensed under the MIT License (MIT).
+ * For details, see: https://github.com/nuintun/beauty-form/blob/master/LICENSE
  */
 
 'use strict';
