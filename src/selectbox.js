a0ca04fae5e8ec0e5062e4dc8a9bb319face7c09
nuintun <nuintun@qq.com>
2016-06-12 17:54:18 +0800

    update files

--- src/selectbox.js
+++ src/selectbox.js
@@ -13,34 +13,109 @@ require('./css/selectbox.css');
 
 var $ = require('jquery');
 
-function SelectBox(){
+var reference = 0;
+var doc = $(document);
 
+/**
+ * proxy
+ * @param callback
+ * @returns {Function}
+ */
+function proxy(callback){
+  return function (element){
+    var context = this;
+
+    element = arguments.length ? $(element) : this.elements;
+
+    element.each(function (){
+      if ($.data(this, 'data-beauty-select')) {
+        callback.call(context, this);
+      }
+    });
+  };
+}
+
+function SelectBox(scope, options){
+  if (!(this instanceof SelectBox)) {
+    return new SelectBox(scope);
+  }
+
+  this.type = 'select';
+
+  if (!scope || !scope.nodeType
+    || (scope.nodeType !== 1
+    && scope.nodeType !== 9
+    && scope.nodeType !== 11)) {
+    scope = document;
+  }
+
+  this.elements = $('select', scope).not('[multiple]');
+
+  this.init();
 }
 
 SelectBox.prototype = {
   init: function (){
-
+    this.beauty();
   },
-  focus: function (){
+  focus: proxy(function (element){
+    $(element.nextSibling).addClass('ui-beauty-select-focus');
+  }),
+  blur: proxy(function (element){
+    $(element.nextSibling).removeClass('ui-beauty-select-focus');
+  }),
+  enable: proxy(function (element){
+    $(element.nextSibling).removeClass('ui-beauty-select-disabled');
+  }),
+  disable: proxy(function (element){
+    $(element.nextSibling).addClass('ui-beauty-select-disabled');
+  }),
+  refresh: proxy(function (element){
+    if (element.disabled) {
+      this.disable(element);
+    } else {
+      this.enable(element);
+    }
 
-  },
-  blur: function (){
-
-  },
-  show: function (){
-
-  },
-  hide: function (){
-
-  },
-  enable: function (){
-
-  },
-  disable: function (){
-
-  },
+    if (document.activeElement === element) {
+      this.focus(element);
+    } else {
+      this.blur(element);
+    }
+  }),
   beauty: function (){
+    var context = this;
 
+    this.elements.each(function (){
+      var element = $(this);
+
+      if (!element.data('data-beauty-select')) {
+        element.css({
+          position: 'absolute',
+          opacity: 0,
+          top: 'auto',
+          right: 'auto',
+          bottom: 'auto',
+          left: 'auto',
+          zIndex: -1
+        });
+
+        var selectbox = $('<div tabindex="-1" class="ui-beauty-select"/>');
+
+        selectbox.insertAfter(element);
+
+        selectbox.css({
+          width: element.outerWidth() - selectbox.outerWidth() - selectbox.width(),
+          height: element.outerHeight() - selectbox.outerHeight() - selectbox.height()
+        });
+
+        reference++;
+
+        element.data('data-beauty-select', true);
+      }
+
+      context.refresh(this);
+    });
   },
   destory: function (){
 
