c3671987730eeabd80e1f1e597992eca514193a1
nuintun <nuintun@qq.com>
2016-06-12 18:23:00 +0800

    update files

--- examples/css/selectbox.css.js
+++ examples/css/selectbox.css.js
@@ -1,4 +1,4 @@
 define("css/selectbox.css.js", ["import-style"], function(require, exports, module){
 var style = require("import-style");
-style.css(".ui-beauty-select {\n  display: inline-block;\n  *display: inline; *zoom: 1;\n  position: relative;\n  margin: 0; padding: 0;\n  border: 1px solid #ccc;\n  background: url(./examples/images/select.png) no-repeat right center;\n}\n");
+style.css(".ui-beauty-select {\n  display: inline-block;\n  *display: inline; *zoom: 1;\n  position: relative;\n  margin: 0; padding: 0;\n  outline: none;\n  border: 1px solid #ccc;\n  background: url(./examples/images/select.png) no-repeat right center;\n}\n.ui-beauty-select-focus {\n  border: 1px solid #09e;\n  /*box-shadow: 0 0 0 1px #a6c8ff;*/\n}\n");
 });
