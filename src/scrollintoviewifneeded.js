cb6716157a2f5c4c5b7979feefe1877db1175bbf
nuintun <nuintun@qq.com>
2016-11-15 16:25:40 +0800

    update files

--- src/scrollintoviewifneeded.js
+++ src/scrollintoviewifneeded.js
@@ -3,6 +3,7 @@
  * Date: 2016/7/15
  * https://github.com/nuintun/beauty-form
  * https://github.com/stipsan/scroll-into-view-if-needed
+ *
  * This is licensed under the MIT License (MIT).
  * For details, see: https://github.com/nuintun/beauty-form/blob/master/LICENSE
  */
